605b4822a4694a3b074f7a04b3cf382505265da5
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-08 11:14:32 -0500

    executable

--- RubyCalc.rb
+++ RubyCalc.rb
@@ -1,7 +1,18 @@
-require_relative "./help"
+#! /usr/bin/env ruby
 
 system "clear"
 print "Welcome to RubyCalc!\n"
+
+def help()
+    system "clear"
+    print "RubyCalc Help\n"
+    print "To use the result of the previous calculation use 'res'\n"
+    print "To clear the thread use 'clear'\n"
+    print "To exit the program use 'quit', or 'exit'\n"
+    print "To see this message again use 'help'\n"
+    print "To report any issue please refer to: 
+    'https://github.com/FenwickElliott/RubyCalc'\n"
+end
 
 while true do
     print "> "
