f0355dcd31545aa16af7de1244e32f569a8bd9b9
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-08 10:57:32 -0500

    help

--- RubyCalc.rb
+++ RubyCalc.rb
@@ -1,14 +1,22 @@
 system "clear"
 print "Welcome to RubyCalc!\n"
-print "To exit type 'exit'\n"
-print "To use the result from the previous calculation use 'res'\n"
 
 while true do
     print "> "
+    input = gets.chomp
+    if input == "help"
+        help
+    end
     begin
-        res = eval gets.chomp
+        res = eval input
         print "= ", res, "\n"
     rescue
         print "I'm sorry, that doesn't compute\n"
     end
+end
+
+def help()
+    print "RubyCalc Help\n"
+    print "use 'help' to see this message again\n"
+    print "To report any issue please refer to: 'https://github.com/FenwickElliott/RubyCalc'\n"
 end
