2f5ddd2d398b16bab8f9f7e805fe854b2cbe3751
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-08 11:06:01 -0500

    help

--- RubyCalc.rb
+++ RubyCalc.rb
@@ -1,22 +1,25 @@
+require_relative "./help"
+
 system "clear"
 print "Welcome to RubyCalc!\n"
 
 while true do
     print "> "
     input = gets.chomp
-    if input == "help"
+    case input.downcase
+    when "help"
         help
+    when "clear"
+        system "clear"
+        print "RubyCalc\n"
+    when "quit"
+        break
+    else
+        begin
+            res = eval input
+            print "= ", res, "\n"
+        rescue
+            print "I'm sorry, that doesn't compute\n"
+        end
     end
-    begin
-        res = eval input
-        print "= ", res, "\n"
-    rescue
-        print "I'm sorry, that doesn't compute\n"
-    end
-end
-
-def help()
-    print "RubyCalc Help\n"
-    print "use 'help' to see this message again\n"
-    print "To report any issue please refer to: 'https://github.com/FenwickElliott/RubyCalc'\n"
 end
